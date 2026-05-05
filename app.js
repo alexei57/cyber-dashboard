8e45d08b58c7be4c2ae7524b50733b8ecbdcc0df
alexei57 <vdovichenko.alexei@gmail.com>
2026-05-05 11:49:55 +0300

    feat: Added mouse tracking and animation loop

---
+++ app.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById('main-canvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+};
+
+resizeCanvas();
+
+window.addEventListener('resize', resizeCanvas);
+
+
+let mouse = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#00f2ff';
+    ctx.fillRect(mouse.x, mouse.y, 50, 50);
+
+    requestAnimationFrame(animate);
+}
+
+animate();
